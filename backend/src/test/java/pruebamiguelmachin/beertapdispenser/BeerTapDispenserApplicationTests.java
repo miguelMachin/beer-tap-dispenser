11902e837d2be620d9793ddab7eb8f4ffec9dc77
miguel <miguel@miguel.com>
2024-08-05 21:06:49 +0200

    test

--- backend/src/test/java/pruebamiguelmachin/beertapdispenser/BeerTapDispenserApplicationTests.java
+++ backend/src/test/java/pruebamiguelmachin/beertapdispenser/BeerTapDispenserApplicationTests.java
@@ -9,62 +9,136 @@ import pruebamiguelmachin.beertapdispenser.domain.model.dto.DispenserDto;
 import pruebamiguelmachin.beertapdispenser.domain.model.dto.DispenserSpendingDto;
 import pruebamiguelmachin.beertapdispenser.domain.model.dto.DispenserStatusDto;
 import pruebamiguelmachin.beertapdispenser.domain.model.enums.DispenserStatusEnum;
+import pruebamiguelmachin.beertapdispenser.infraestructura.adapter.exception.DispenserException;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BeerTapDispenserApplicationTests {
-/*
-	@Autowired
-	DispenserService dispenserService;
-@Test
-	void contextLoads() {
+
+    @Autowired
+    DispenserService dispenserService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    /*Services*/
+    @Test
+    void testDispenserServiceCreateDispenser() {
+        DispenserDto dispenserDto = dispenserService.createDispenser(0.3f, 1.0f);
+        assertThat(dispenserDto.getFlow_volume())
+                .isEqualTo(0.3f);
+    }
+
+    @Test
+    void testDispenserServiceFindById() {
+        DispenserDto dispenserDto = dispenserService.findDispenser("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb");
+        assertThat(dispenserDto.getId())
+                .isEqualTo("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb");
+    }
+
+    @Test
+    void testDispenserServiceGetAllDispenser() {
+        List<DispenserDto> dispenserDto = dispenserService.findAll();
+
+        assertThat(dispenserDto.size())
+                .isGreaterThan(0);
+    }
+
+    @Test
+    void testDispenserServiceFindByIdNotFound() {
+        assertThrows(DispenserException.class, () -> dispenserService.findDispenser("cb0825b0-9666-4fb9-ac79-c5125c332b7"));
+
+    }
+
+    //The following tests must run one after the other, care with order of execution
+
+    @Test
+    void testDispenserServiceUpdateStatusCloseException() throws ParseException {
+        DispenserStatusDto dispenserStatusDto = new DispenserStatusDto();
+        dispenserStatusDto.setStatus(DispenserStatusEnum.CLOSE.ordinal());
+        dispenserStatusDto.setUpdated_at("2024-08-04T01:42:15.781Z");
+
+        assertThrows(DispenserException.class, () -> dispenserService.updateStatus("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb", dispenserStatusDto));
+    }
+
+    @Test
+    void testDispenserServiceUpdateStatusOpen() throws ParseException {
+        DispenserStatusDto dispenserStatusDto = new DispenserStatusDto();
+        dispenserStatusDto.setStatus(DispenserStatusEnum.OPEN.ordinal());
+        dispenserStatusDto.setUpdated_at("2024-08-04T01:42:15Z");
+        dispenserService.updateStatus("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb", dispenserStatusDto);
+        DispenserDto dispenserDto = dispenserService.findDispenser("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb");
+
+        assertAll(
+                "Grouped Assertions of DispenserUpdateStatus open",
+                () -> assertEquals(DispenserStatusEnum.OPEN.ordinal(), dispenserDto.getStatus(), "Status should be 1"),
+                () -> assertThat(dispenserDto.getUsages().size()).isGreaterThan(0),
+                () -> assertNull(dispenserDto.getUsages().get(0).getClose_at(), "Close Date should be null")
+        );
+    }
+
+    @Test
+    void testDispenserServiceUpdateStatusClose() throws ParseException {
+        DispenserStatusDto dispenserStatusDto = new DispenserStatusDto();
+        dispenserStatusDto.setStatus(DispenserStatusEnum.CLOSE.ordinal());
+        dispenserStatusDto.setUpdated_at("2024-08-04T01:42:25Z");
+        dispenserService.updateStatus("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb", dispenserStatusDto);
+        DispenserDto dispenserDto = dispenserService.findDispenser("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb");
+
+        assertAll(
+                "Grouped Assertions of DispenserUpdateStatus close",
+                () -> assertEquals(DispenserStatusEnum.CLOSE.ordinal(), dispenserDto.getStatus(), "Status should be 1"),
+                () -> assertThat(dispenserDto.getUsages().size()).isGreaterThan(0),
+                () -> assertNotNull(dispenserDto.getUsages().get(0).getClose_at(), "Close Date dont should be null"),
+                () -> assertEquals(10, dispenserDto.getAmount(), "Amount should be 10")
+        );
+    }
+
+
+    @Test
+    void testDispenserServiceUpdateStatusCloseNotFoundUsage() throws ParseException {
+        DispenserStatusDto dispenserStatusDto = new DispenserStatusDto();
+        dispenserStatusDto.setStatus(DispenserStatusEnum.CLOSE.ordinal());
+        dispenserStatusDto.setUpdated_at("2024-08-04T01:42:25Z");
+
+        assertThrows(DispenserException.class, () -> dispenserService.updateStatus("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb", dispenserStatusDto));
+
+    }
+
+
+    @Test
+    void testDispenserServiceFindDispenserSpending() {
+        DispenserSpendingDto dispenserSpendingDto = dispenserService.findDispenserSpending("fe749b90-5f8c-4ba7-82e2-1331bfff7dcb");
+        assertAll(
+                "Grouped Assertions of DispenserSpending",
+                () -> assertThat(dispenserSpendingDto.getUsages().size()).isGreaterThan(0),
+                () -> assertEquals(10, dispenserSpendingDto.getAmount(), "Amount should be 10")
+        );
+
+    }
+
+
+    /*Utils*/
+	@Test
+	void testUtilsLongDateToString() throws ParseException {
+		String dateString = Utils.longDateToString(Long.parseLong("1704088800000"));
+        assertThat(dateString)
+                .isEqualTo("2024-01-01T07:00:00Z");
+
 	}
 
-	@Test
-	void testDispenserServiceCreateDispenser(){
-		DispenserDto dispenserDto = dispenserService.createDispenser(0.3f, 1.0f);
-		System.out.println(dispenserDto);
-	}
+    @Test
+    void testUtilsStringToDate() throws ParseException {
+        Date date = Utils.stringToDate("2024-01-01T07:00:00Z");
+        assertThat(date.toString())
+                .isEqualTo("Mon Jan 01 07:00:00 CET 2024");
 
-	@Test
-	void testDispenserServiceFindById() {
-		DispenserDto dispenserDto = dispenserService.findDispenser("cb0825b0-9666-4fb9-ac79-c5125c332b7e");
-		System.out.println(dispenserDto);
-	}*/
-/*
-	@Test
-	void testDispenserServiceFindByIdSpending() throws ParseException {
-		DispenserSpendingDto dispenserSpendingDto = dispenserService.findDispenserSpending("f4b94168-e520-44ef-a6b0-21ebcfa568a2");
-		System.out.println(dispenserSpendingDto);
-
-    //     simpleDateFormat.format(calendar.getTime());
-
-        String date_string = "2024-01-01 07:00:00";
-        //Instantiating the SimpleDateFormat class
-        //SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        //Parsing the given String to Date object
-        Date date = formatter.parse(date_string);
-        System.out.println("Date value: "+date);
-        System.out.println("Date value: "+date.toString());
-        System.out.println("Date value: "+date.getTime());
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(Long.parseLong("1704088800000"));
-        System.out.println(formatter.format(calendar.getTime()));
-        System.out.println(formatter.format(date.getTime()));
-
-        //System.out.println(Utils.longDateToString(Long.parseLong("1704088800000")));
-		//System.out.println(Utils.stringToDate("2024-01-01T06:00:20Z"));
-
-
-
-
-	}*/
-
-
+    }
 }
